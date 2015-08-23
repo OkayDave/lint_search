54fbd1aebffee6b830465223ec8ed868ee1b542c
Dave Russell <dave.kerr@gmail.com>
2015-08-23 16:40:27 +0100

    added lint feature

--- lib/lint_search/lint.rb
+++ lib/lint_search/lint.rb
@@ -1,15 +1,38 @@
-
+require 'rubygems'
+require 'mechanize'
 
 module LintSearch
   class Lint
 
-    attr_reader :search_result
+    attr_reader :search_result, :linter_url, :css_selector
 
-    def initialize(search_result)
-      @search_result = search_result
+    def initialize(options)
+      @search_result = options[:search_result]
+      @linter_url    = options[:linter_url]
+      @css_selector  = options[:css_selector]
 
       raise Exception.new("Lint: search_result is required") if @search_result.nil?
       raise Exception.new("Lint: search_result must be a LintSearch::SearchResult") unless @search_result.is_a?(LintSearch::SearchResult)
+      raise Exception.new("Lint: linter_url is required") if @linter_url.nil?
+      raise Exception.new("Lint: css_selector is required") if @css_selector.nil?
+    end
+
+    def lint
+      robot = Mechanize.new
+      robot.user_agent_alias = 'Linux Firefox'
+
+      robot.get(@linter_url) do |page|
+        form = page.forms.first
+        form.texts.first.value = @search_result.url
+        results_page = form.submit
+      
+        lint_results = []
+        results_page.search(@css_selector).each do |item|
+          lint_results << item.text
+        end
+        
+        return lint_results
+      end
     end
   end
 
--- spec/lint_spec.rb
+++ spec/lint_spec.rb
@@ -1,11 +1,14 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "LintSearch::Lint" do
-  let(:search_result) { LintSearch::SearchResult.new(url: "http://www.bbc.co.uk/", title: "BBC") }
+  let(:search_result) { { search_result: LintSearch::SearchResult.new(url: "http://www.bbc.co.uk/", title: "BBC") } }
+  let(:params_good)   { { linter_url: "http://validator.w3.org", css_selector: "li.info" } }
+  let(:params_no_url) { { css_selector: "li.info" } }
+  let(:params_no_css_selector) { { linter_url: "http://validator.w3.org" } }
 
   context ".new" do
     context "with valid data" do
-      let(:lint) { LintSearch::Lint.new(search_result) }
+      let(:lint) { LintSearch::Lint.new(search_result.merge(params_good)) }
       
       it "creates a new Lint object" do
         expect(lint).to be_a(LintSearch::Lint)
@@ -13,18 +16,34 @@ describe "LintSearch::Lint" do
 
       it "sets the search_result variable" do
         expect(lint.search_result).to be_a(LintSearch::SearchResult)
-        expect(lint.search_result).to eq(search_result)
+        expect(lint.search_result).to eq(search_result[:search_result])
+      end
+
+      it "sets the linter_url variable" do
+        expect(lint.linter_url).to eq(params_good[:linter_url])
+      end
+
+      it "sets the css_selector variable" do
+        expect(lint.css_selector).to eq(params_good[:css_selector])
       end
     end
 
     context "with invalid data" do
       
       it "raises an exception when no search result is passed" do
-        expect{LintSearch::Lint.new(nil)}.to raise_exception(/required/i)
+        expect{LintSearch::Lint.new(params_good)}.to raise_exception(/search_result/i)
       end
 
       it "raises an exception when search result is not a SearchResult" do
-        expect{LintSearch::Lint.new(10)}.to raise_exception(/SearchResult/i)
+        expect{LintSearch::Lint.new(params_good.merge({search_result: 10}))}.to raise_exception(/LintSearch::SearchResult/i)
+      end
+
+      it "raises an exception when no linter url is passed" do
+        expect{LintSearch::Lint.new(params_no_url.merge(search_result))}.to raise_exception(/linter_url/i)
+      end
+
+      it "raises an exception when no css_selector is passed" do
+        expect{LintSearch::Lint.new(params_no_css_selector.merge(search_result))}.to raise_exception(/css_selector/i)
       end
   
     end
